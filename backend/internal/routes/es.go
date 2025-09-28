d342956c10edf01c7ec2dfb49bbbb7eabbc73e5c
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-28 19:01:08 +0530

    routes changes

--- backend/internal/routes/es.go
+++ backend/internal/routes/es.go
@@ -1,7 +1,28 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
 
-func Get(w http.ResponseWriter, r *http.Request) (err error) {
-	return nil
+	"github.com/DivyanshuShekhar55/yellow-monkey/backend/internal/es"
+)
+
+func (h *Handler) GetAllGroups(w http.ResponseWriter, r *http.Request) {
+	res, err := es.GetAllGroups(r.Context(), h.ESConn)
+
+	if err != nil {
+		http.Error(w, "error getting all groups", http.StatusInternalServerError)
+		log.Printf("error: %s", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err = json.NewEncoder(w).Encode(&res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 }
+
+func (h *Handler) CreateUserIndex() {}
--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,9 +1,25 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
 
-func Register(mux *http.ServeMux) {
+	"github.com/elastic/go-elasticsearch/v9"
+)
+
+type Handler struct {
+	ESConn *elasticsearch.Client
+}
+
+func NewHandler(esconn *elasticsearch.Client) *Handler {
+	return &Handler{
+		ESConn: esconn,
+	}
+}
+
+func (h *Handler) Register(mux *http.ServeMux) {
 	mux.HandleFunc("GET /health", func( w http.ResponseWriter, r *http.Request,) {
 		w.Write([]byte("ok"))
 	})
+	
+	mux.HandleFunc("/groups", h.GetAllGroups)
 }
